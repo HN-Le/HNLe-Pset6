6f0cafb53fe20c9b6baad316aafe822dd00adcfa
HN-Le <hy.nhu.le@gmail.com>
2017-06-18 21:21:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hnle_pset6/FourthActivity.java
@@ -0,0 +1,15 @@
+package com.example.hnle_pset6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+    }
+
+    // add menu icon for settings! TODO
+}
